7789edc15142734bd85a519bf5376a8f8caf92cd
Martin Le Guillou <martin.leguillou@y-nov.com>
2014-11-27 00:20:44 +0100

    test appel api

--- js/callOfBeer.js
+++ js/callOfBeer.js
@@ -16,12 +16,16 @@ var callOfBeerApp = angular.module('callOfBeerApp', [
  */
 var mapMod = angular.module('mapCob',['leaflet-directive']);
 
-mapMod.controller("mapCtrl", [ "$scope", function($scope) {
-    angular.extend($scope, {
-        center: {
-            lat: 46.25,
-            lng: 2.1,
-            zoom: 6
-        }
-    });
+mapMod.controller("mapCtrl", [ "$scope", '$http', function($scope, $http) {
+	$scope.center = {
+        lat: 46.25,
+        lng: 2.1,
+        zoom: 6
+    };
+
+    var marquers = $http.get('http://api.callofbeer.com/events?topLat=45&topLon=-1&botLat=44&botLon=0').success(successCallback);
+
+    var successCallback = function(data, status, headers, config) {
+    	console.log(data);
+    }
 }]);
